a3866985d4a598ae27bf5b652f057b679cbc5612
khan1203 <rk.rifat1203@gmail.com>
2025-07-21 16:15:09 +0600

    PATCH method disallowed

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, exceptions
 from .models import Team, Task
 from .serializers import TeamSerializer, TaskSerializer
 
@@ -9,6 +9,12 @@ class TeamViewset(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+    def partial_update(self, request, *args, **kwargs):
+        raise exceptions.MethodNotAllowed('PATCH')
+
 class TaskViewset(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        raise exceptions.MethodNotAllowed('PATCH')
