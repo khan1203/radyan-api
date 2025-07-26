7800d12394c7347de228f666acaeaf94499fc46d
khan1203 <rk.rifat1203@gmail.com>
2025-07-26 12:40:12 +0600

    debug toggled

--- radyan_rest_main/settings.py
+++ radyan_rest_main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e+83nt6zrxgmug(lo42e+yz#b=u0t^1c+mxv!olvy97=1i_i&#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']  # You can change this to your Render domain later
 
